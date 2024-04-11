92883acfedc7d7bfd93872c9d8f4ddd9b1015f84
matteocenci <matteocenci01@gmail.com>
2024-04-11 15:44:51 +0200

    feat: milestone 0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 // Milestone 0: creare l'array di oggetti con le informazioni fornite
+const employees = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg"
+        
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        img: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        img: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg"
+        
+    }
+]
+
+console.log(employees);
+console.log(employees[0]);
